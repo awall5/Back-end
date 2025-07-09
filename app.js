d257e7155efd8840a9d9bee4063a0618d722c588
awall5 <awalmanga25@gmail.com>
2025-07-09 14:22:04 +0530

    Back-End

---
+++ app.js
@@ -0,0 +1,27 @@
+const dotEnv = require("dotenv");
+dotEnv.config();
+
+const express = require("express");
+require("./Config/db.js");
+
+const { apiRouter } = require("./api/v1/routes.js");
+
+
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("------------");
+    console.log(new Date(), req.method, req.url);
+    console.log("------------");
+    next();
+});
+
+app.use("/api/v1", apiRouter);
+
+app.listen(2900, () => {
+    console.log("------ Server is running --------");
+});
